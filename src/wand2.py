1149a88a56846cf57b95e93c5fc6800a758fbcd0
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-01-15 17:42:32 +0900

    Update wand2.py

--- src/wand2.py
+++ src/wand2.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template, Response, stream_with_context
+from flask import Flask, request, render_template, Response, stream_with_context
 from threading import Thread
 from lab.streamer import Streamer
 
 app = Flask(__name__)
-app_2 = Flask(__name__)
 
 version = '0.1.0'
 
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('start_count.html')
 
 streamer = Streamer()
 
@@ -29,21 +28,10 @@ def stream_gen(src):
         print('[wandlab]', 'disconnected stream')
         streamer.stop()
 
-def run_app(app, port):
-    app.run(host='192.168.1.144', port=port)
 
 if __name__ == '__main__':
     print('------------------------------------------------')
     print('Wandlab CV - version ' + version)
     print('------------------------------------------------')
     
-    # 멀티쓰레딩으로 두 개의 애플리케이션을 동시에 실행
-    thread_app1 = Thread(target=run_app, args=(app, 5000))
-    thread_app2 = Thread(target=run_app, args=(app_2, 7000))
-
-    thread_app1.start()
-    thread_app2.start()
-    
-    # 두 쓰레드가 종료될 때까지 대기
-    thread_app1.join()
-    thread_app2.join()
+    app.run(host='192.168.1.144',port=5000)
