f654cc3f1298ff973b6224cb79e59ec46cf8b077
printfmyname <143148956+printfmyname@users.noreply.github.com>
2024-01-15 21:22:51 +0900

    Update wand1.py

--- src/wand1.py
+++ src/wand1.py
@@ -1,32 +1,32 @@
 from flask import Flask, request, render_template, Response, stream_with_context
 from threading import Thread
-from lab.streamer import Streamer
+from lab.streamer_2 import Streamer3
 
 app = Flask(__name__)
 
 version = '0.1.0'
 
-@app.route('/index')
+@app.route('/index2')
 def index():
-    return render_template('start_count.html')
+    return render_template('squat.html')
 
-streamer = Streamer()
+streamer2 = Streamer2()
 
-@app.route('/stream')
-def stream():
-    src = request.args.get('src', default=0, type=int)
-    return Response(stream_with_context(stream_gen(src)), mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/stream2')
+def stream2():
+    src2 = request.args.get('src', default=0, type=int)
+    return Response(stream_with_context(stream2_gen(src)), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def stream_gen(src):
+def stream_gen(src2):
     try:
-        streamer.run(src)
+        streamer2.run(src2)
         while True:
-            frame = streamer.bytescode()
+            frame = streamer2.bytescode()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
     except GeneratorExit:
         print('[wandlab]', 'disconnected stream')
-        streamer.stop()
+        streamer2.stop()
 
 
 if __name__ == '__main__':
