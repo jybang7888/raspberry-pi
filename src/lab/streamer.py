6db5731f6392faad6ad7d33421e5a37508807046
printfmyname <143148956+printfmyname@users.noreply.github.com>
2024-01-16 14:08:40 +0900

    Update streamer.py

--- src/lab/streamer.py
+++ src/lab/streamer.py
@@ -161,7 +161,7 @@ class Streamer :
                         cv2.circle(image, (lmList[26][1], lmList[26][2]), 15, (0, 0, 255), cv2.FILLED)
                         cv2.circle(image, (lmList[27][1], lmList[27][2]), 15, (0, 0, 255), cv2.FILLED)
                         cv2.circle(image, (lmList[28][1], lmList[28][2]), 15, (0, 0, 255), cv2.FILLED)
-                        if (lmList[12][2] and lmList[11][2] >= lmList[14][2] and lmList[13][2]) and (angle_1 or angle_4 < 90) and (angle_2 or angle_5 < 180) and (angle_2 or angle_5 > 150) :
+                        if (lmList[12][2] and lmList[11][2] >= lmList[14][2] and lmList[13][2]) and (angle_1 < 90) and (angle_2 < 180) and (angle_2 > 150) :
                             cv2.circle(image, (lmList[11][1], lmList[11][2]), 15, (0, 255, 0), cv2.FILLED)
                             cv2.circle(image, (lmList[12][1], lmList[12][2]), 15, (0, 255, 0), cv2.FILLED)
                             cv2.circle(image, (lmList[13][1], lmList[13][2]), 15, (0, 255, 0), cv2.FILLED)
