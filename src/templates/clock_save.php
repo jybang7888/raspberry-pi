5b19d1e2701e3844cd3d7b9ab41b9ccde2cf096d
jybang7888 <151609903+jybang7888@users.noreply.github.com>
2024-01-21 23:07:29 +0900

    clock_save.php

--- src/templates/clock_save.php
+++ src/templates/clock_save.php
@@ -1,23 +1,23 @@
 <?php
 $total = 0;
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $endtime = $_POST["currentTime"];
-}
-$starttime = clone $endtime;
-$starttime -> modify('-1 minute');
-
 $servername = "192.168.1.144";
 $username = "root";
 $password = "1234";
 $dbname = "health";
+
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM squat WHERE datetime > $starttime AND datetime < $endtime";
-$result = $conn->query($sql);
+$endtime = $_POST['currentTimeString'];
+$starttime = clone $endtime;
+$starttime -> modify('-1 minute');
 
+$sql = "SELECT * FROM squat WHERE datetime > '$starttime' AND datetime < '$endtime'";
+
+$result = $conn->query($sql);
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         if()
@@ -25,8 +25,8 @@ if ($result->num_rows > 0) {
     }
 }
 
-$sql = "INSERT INTO heal (name, total) VALUES ('11', '$total')";
-$conn->query($sql);
+$sql2 = "INSERT INTO heal (name, total) VALUES ('11', '$total')";
+$conn->query($sql2);
 
 $conn->close();
 ?>
