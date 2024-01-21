9b361a5abfea66bd72d785b45558e100d41375b3
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-01-21 21:21:31 +0900

    Update DB.py

--- src/DB.py
+++ src/DB.py
@@ -4,12 +4,17 @@ class Database():
     def __init__(self):
         self.db = pymysql.connect(host = 'localhost', user = 'root', password='1234',db='health',charset='utf8')
         self.cursor = self.db.cursor()
-    def show(self):
+    def show_1(self):
         sql = "SELECT * from push_up"
+        self.cursor.execute(sql)
+        result = self.cursor.fetchall()
+        return(result)
+    def show_2(self):
+        sql = "SELECT * from squat"
         self.cursor.execute(sql)
         result = self.cursor.fetchall()
         return(result)
 
 if __name__ == "__main__":
   db = Database()
-  db.show()
+  db.show_1()
