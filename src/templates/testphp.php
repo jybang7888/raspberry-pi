f445e885b029bbabddabd544d960ceea25cd5b09
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-01-28 16:30:30 +0900

    Update testphp.php

--- src/templates/testphp.php
+++ src/templates/testphp.php
@@ -5,5 +5,12 @@ $username = "root";
 $password = "1234";
 $dbname = "health";
 
-error_log("Form submitted from clock_save.php", 0);
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql1 = "INSERT INTO heal (name, total) VALUES ('11', '2')";
+$conn->query($sql1);
 ?>
