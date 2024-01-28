0ce6e76c9449fc9827b657807a05623170a39220
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-01-28 16:22:45 +0900

    Update testphp.php

--- src/templates/testphp.php
+++ src/templates/testphp.php
@@ -1,3 +1,9 @@
 <?php
-phpinfo();
+$total = 0;
+$servername = "192.168.1.144";
+$username = "root";
+$password = "1234";
+$dbname = "health";
+
+error_log("Form submitted from clock_save.php", 0);
 ?>
