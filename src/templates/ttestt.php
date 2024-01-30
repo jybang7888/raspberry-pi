7e51e59d1611caa75b07c0b4132f8ad45bdb989f
jybang7888 <151609903+jybang7888@users.noreply.github.com>
2024-01-30 21:31:52 +0900

    ttestt.php

--- src/templates/ttestt.php
+++ src/templates/ttestt.php
@@ -7,24 +7,35 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-    $name = $_POST['username'];
-    $hours = $_POST['hours'];
-    $minutes = $_POST['minutes'];
-    $seconds = $_POST['seconds'];
+    $starthours = $_POST['starthours'];
+    $startminutes = $_POST['startminutes'];
+    $startseconds = $_POST['startseconds'];
+    $endhours = $_POST['endhours'];
+    $endminutes = $_POST['endminutes'];
+    $endseconds = $_POST['endseconds'];
+    $year = $_POST['year'];
+    $month = $_POST['month'];
+    $day = $_POST['day'];
+    
+    $starthours = sprintf("%02d", $starthours);
+    $startminutes = sprintf("%02d", $startminutes);
+    $startseconds = sprintf("%02d", $startseconds);
+    $endhours = sprintf("%02d", $endhours);
+    $endminutes = sprintf("%02d", $endminutes);
+    $endseconds = sprintf("%02d", $endseconds);
+    $year = sprintf("%04d", $year);
+    $month = sprintf("%02d", $month);
+    $day = sprintf("%02d", $day);
+    
+    $endtime = $endhours . ':' . $endminutes . ':' . $endseconds;
+    $starttime = $starthours . ':' . $startminutes . ':' . $startseconds;
+    $date = $year . '-' . $month . '-' . $day;
 
-    $hours = sprintf("%02d", $hours);
-    $minutes = sprintf("%02d", $minutes);
-    $seconds = sprintf("%02d", $seconds);
-
-    $endtime = $hours . ':' . $minutes . ':' . $seconds;
-    $starttime = clone $endtime;
-    $starttime -> modify('-1 minute');
-
-    $sql = "SELECT * FROM squat WHERE datetime > '$starttime' AND datetime < '$endtime'";
+    $sql = "SELECT * FROM squat WHERE datetime > '$starttime' AND datetime < '$endtime' AND state = 'down'";
     $result = mysqli_query($conn, $sql);
     $row_count = mysqli_num_rows($result);
     
-    $sql1 = "INSERT INTO heal (name, total) VALUES ('$name', '$row_count')";
+    $sql1 = "INSERT INTO heal (name, date, time, total) VALUES ('jun', '$date', '$starttime', '$row_count')";
     $conn->query($sql1);
     
     header("localhost");
