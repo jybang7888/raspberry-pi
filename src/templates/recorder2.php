7a2fbc7a47f41be5c3ab1e607e3831648b2f5bd9
jybang7888 <151609903+jybang7888@users.noreply.github.com>
2024-02-04 23:55:08 +0900

    recorder2.php

--- src/templates/recorder2.php
+++ src/templates/recorder2.php
@@ -21,49 +21,37 @@ $result2 = $conn->query($sql2);
 $sql3 = "SELECT * FROM total_burpee WHERE name = '$name' ORDER BY date DESC, start_time DESC LIMIT $count";
 $result3 = $conn->query($sql3);
 
-header("Location: http://192.168.1.144:5000/record");
-echo "Data of " .$name. "<hr><hr>";
-    
-if ($exercise == 'PushUp' or $exercise == 'Total Exercises'){
-	if(isset($result1) && $result1->num_rows > 0){
-	
-	    while($row = $result1->fetch_assoc()){
-	        echo "Date: ".$row['date']."<br>";
-	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
-	        echo "Total count: ".$row['total']."<hr>"; 
-	    }
-	}
-	else{
-	    echo "No data about pushup.";
-	}
+if(isset($result1) && $result1->num_rows > 0){
+    while($row = $result1->fetch_assoc()){
+        echo "Date: ".$row['date']."<br>";
+        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
+        echo "Total count: ".$row['total']."<hr>"; 
+    }
 }
-echo "<hr>";
-if ($exercise == 'Squat' or $exercise == 'Total Exercises'){
-	if(isset($result2) && $result2->num_rows > 0){
-	    while($row = $result2->fetch_assoc()){
-	        echo "Date: ".$row['date']."<br>";
-	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
-	        echo "Total count: ".$row['total']."<hr>"; 
-	    }
-	}
-	else{
-	    echo "No data about squat.";
-	}
+else{
+    echo "No data about pushup.";
 }
-echo "<hr>";
-if ($exercise == 'Burpee' or $exercise == 'Total Exercises'){
-	if(isset($result3) && $result3->num_rows > 0){
-	    while($row = $result3->fetch_assoc()){
-	        echo "Date: ".$row['date']."<br>";
-	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
-	        echo "Total count: ".$row['total']."<hr>"; 
-	    }
-	}
-	else{
-	    echo "No data about Burpee.";
-	}
+if(isset($result2) && $result2->num_rows > 0){
+    while($row = $result2->fetch_assoc()){
+        echo "Date: ".$row['date']."<br>";
+        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
+        echo "Total count: ".$row['total']."<hr>"; 
+    }
+}
+else{
+    echo "No data about squat.";
+}
+if(isset($result3) && $result3->num_rows > 0){
+    while($row = $result3->fetch_assoc()){
+        echo "Date: ".$row['date']."<br>";
+        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
+        echo "Total count: ".$row['total']."<hr>"; 
+    }
+}
+else{
+    echo "No data about Burpee.";
 }
 
+header("Location: http://192.168.1.144:5000/record?name=$name&exercise=$exercise&");
 $conn->close();
-
 ?>
