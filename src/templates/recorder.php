afa9de30c837401c093286e4b13fb6f6b03270ad
jybang7888 <151609903+jybang7888@users.noreply.github.com>
2024-02-04 22:17:43 +0900

    recorder.php

--- src/templates/recorder.php
+++ src/templates/recorder.php
@@ -14,25 +14,53 @@ $name = $_GET['name'];
 $exercise = $_GET['exercise'];
 $count = $_GET['count'];
 
-$sql2 = "SELECT * FROM total_pushup WHERE name = '$name' LIMIT '$count'";
+$sql1 = "SELECT * FROM total_pushup WHERE name = '$name' ORDER BY date DESC, start_time DESC LIMIT $count";
+$result1 = $conn->query($sql1);
+$sql2 = "SELECT * FROM total_squat WHERE name = '$name' ORDER BY date DESC, start_time DESC LIMIT $count";
 $result2 = $conn->query($sql2);
+$sql3 = "SELECT * FROM total_burpee WHERE name = '$name' ORDER BY date DESC, start_time DESC LIMIT $count";
+$result3 = $conn->query($sql3);
+echo "Data of " .$name. "<hr><hr>";
+    
+if ($exercise == 'PushUp' or $exercise == 'Total Exercises'){
+	if(isset($result1) && $result1->num_rows > 0){
+	    while($row = $result1->fetch_assoc()){
+	        echo "Date: ".$row['date']."<br>";
+	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
+	        echo "Total count: ".$row['total']."<hr>"; 
+	    }
+	}
+	else{
+	    echo "No data about pushup.";
+	}
+}
+echo "<hr>";
+if ($exercise == 'Squat' or $exercise == 'Total Exercises'){
+	if(isset($result2) && $result2->num_rows > 0){
+	    while($row = $result2->fetch_assoc()){
+	        echo "Date: ".$row['date']."<br>";
+	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
+	        echo "Total count: ".$row['total']."<hr>"; 
+	    }
+	}
+	else{
+	    echo "No data about squat.";
+	}
+}
+echo "<hr>";
+if ($exercise == 'Burpee' or $exercise == 'Total Exercises'){
+	if(isset($result3) && $result3->num_rows > 0){
+	    while($row = $result3->fetch_assoc()){
+	        echo "Date: ".$row['date']."<br>";
+	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
+	        echo "Total count: ".$row['total']."<hr>"; 
+	    }
+	}
+	else{
+	    echo "No data about Burpee.";
+	}
+}
 
-if(isset($result2) && $result2->num_rows > 0){
-    echo "Data of " .$name. "<hr><hr>";
-    $dataRows = array();
-    while($row = $result2->fetch_assoc()){
-        $dataRows[] = $row;
-    }
-    $dataRows = array_reverse($dataRows);
-    while($row = $result2->fetch_assoc()){
-        echo "Date: ".$row['date']."<br>";
-        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
-        echo "Total count: ".$row['total']."<hr>"; 
-    }
-}
-else{
-    echo "No data about pushup.";
-}
 $conn->close();
 
 ?>
