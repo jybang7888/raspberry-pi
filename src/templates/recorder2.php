b98c423af1aae0fb3400f14d4633ff4a508c8251
jybang7888 <151609903+jybang7888@users.noreply.github.com>
2024-02-05 00:03:39 +0900

    recorder2.php

--- src/templates/recorder2.php
+++ src/templates/recorder2.php
@@ -21,6 +21,47 @@ $result2 = $conn->query($sql2);
 $sql3 = "SELECT * FROM total_burpee WHERE name = '$name' ORDER BY date DESC, start_time DESC LIMIT $count";
 $result3 = $conn->query($sql3);
 
+$twoDimArray1 = array();
+$twoDimArray2 = array();
+$twoDimArray3 = array();
+
+$rows = 3;
+$cols = 3;
+
+// 2차원 배열에 빈 배열을 추가하여 초기화
+for ($i = 0; $i < $rows; $i++) {
+    $twoDimArray[$i] = array();
+}
+
+// 예시: 값을 추가하기 전에 2차원 배열 출력
+echo "<pre>";
+print_r($twoDimArray);
+echo "</pre>";
+
+// 나중에 값을 넣을 위치를 지정한 후 값을 할당
+$twoDimArray[0][0] = 1;
+$twoDimArray[0][1] = 2;
+$twoDimArray[0][2] = 3;
+$twoDimArray[1][0] = 4;
+$twoDimArray[1][1] = 5;
+$twoDimArray[1][2] = 6;
+$twoDimArray[2][0] = 7;
+$twoDimArray[2][1] = 8;
+$twoDimArray[2][2] = 9;
+
+// 값이 할당된 2차원 배열 출력
+echo "<pre>";
+print_r($twoDimArray);
+echo "</pre>";
+
+
+
+
+
+
+
+
+
 if(isset($result1) && $result1->num_rows > 0){
     while($row = $result1->fetch_assoc()){
         echo "Date: ".$row['date']."<br>";
