5482d0f84de82d0ac7112bdf0bda1c156abb5a3e
jybang7888 <151609903+jybang7888@users.noreply.github.com>
2024-02-04 19:45:32 +0900

    recorder.php

--- src/templates/recorder.php
+++ src/templates/recorder.php
@@ -10,20 +10,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$username = $_POST['username'];
+$name = $_GET['name'];
 
-$sql2 = "SELECT * FROM total_squat WHERE name = '$username'";
+$sql2 = "SELECT * FROM total_pushup WHERE name = '$name'";
 $result2 = $conn->query($sql2);
 
 if(isset($result2) && $result2->num_rows > 0){
+    echo "Data of " .$name. "<hr><hr>";
     while($row = $result2->fetch_assoc()){
         echo "Date: ".$row['date']."<br>";
-        echo "Time: From ".$row['starttime']." to ".$row['endtime']."<br>";
+        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
         echo "Total count: ".$row['total']."<hr>"; 
     }
 }
 else{
-    echo "No data about squat.";
+    echo "No data about pushup.";
 }
+$conn->close();
 
 ?>
