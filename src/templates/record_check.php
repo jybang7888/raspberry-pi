35e46c89fb43395129cbdbea818bb57c8368c928
jybang7888 <151609903+jybang7888@users.noreply.github.com>
2024-02-05 16:33:03 +0900

    Update record_check.php

--- src/templates/record_check.php
+++ src/templates/record_check.php
@@ -13,16 +13,17 @@ if ($conn->connect_error) {
 $username = $_POST['username'];
 $password = $_POST['password'];
 $exercise = $_POST['exercise'];
-$count = $_POST['count'];
+$date1 = $_POST['date1'];
+$date2 = $_POST['date2'];
 
 $sql = "SELECT * FROM userinfo WHERE username = '$username' AND password = '$password'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    header("Location: record_show.php?name=$username&exercise=$exercise&count=$count");
+    header("Location: record_show.php?name=$username&exercise=$exercise&date1=$date1&date2=$date2");
     exit;
 } else {
-    header("Location: http://192.168.1.249:5000/record_wrong.html");
+    header("Location: record_wrong.html");
 }
 
 $conn->close();
