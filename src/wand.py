2f7a609a023ffb8ca346a54e0966c1f5b46cea74
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-02-05 15:29:14 +0900

    Update wand.py

--- src/wand.py
+++ src/wand.py
@@ -120,4 +120,4 @@ if __name__ == '__main__':
     print('Wandlab CV - version ' + version)
     print('------------------------------------------------')
     
-    app.run(host='192.168.1.144',port=5000)
+    app.run(host='192.168.1.249',port=5000)
