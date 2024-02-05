38fe72ca480aa9f8298a5c5fee254e82674cfe40
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-02-05 12:32:50 +0900

    Update record_show.php

--- src/templates/record_show.php
+++ src/templates/record_show.php
@@ -21,45 +21,48 @@ $result2 = $conn->query($sql2);
 $sql3 = "SELECT * FROM total_burpee WHERE name = '$name' ORDER BY date DESC, start_time DESC LIMIT $count";
 $result3 = $conn->query($sql3);
 
-echo "Data of " .$name. "<hr><hr>";
+echo "Data of " .$name. "<hr><hr><br>";
     
 if ($exercise == 'PushUp' or $exercise == 'Total Exercises'){
+	echo "<strong>Push up</strong> <hr>"
 	if(isset($result1) && $result1->num_rows > 0){
 	
 	    while($row = $result1->fetch_assoc()){
-	        echo "Date: ".$row['date']."<br>";
+	        echo "Date: ".$row['date']." ";
 	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
 	        echo "Total count: ".$row['total']."<hr>"; 
 	    }
 	}
 	else{
-	    echo "No data about pushup.";
+	    echo "No data about pushup. <hr>";
 	}
 }
-echo "<hr>";
+echo "<br><hr>";
 if ($exercise == 'Squat' or $exercise == 'Total Exercises'){
+	echo "<strong>Squat</strong> <hr>"
 	if(isset($result2) && $result2->num_rows > 0){
 	    while($row = $result2->fetch_assoc()){
-	        echo "Date: ".$row['date']."<br>";
+	        echo "Date: ".$row['date']." ";
 	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
 	        echo "Total count: ".$row['total']."<hr>"; 
 	    }
 	}
 	else{
-	    echo "No data about squat.";
+	    echo "No data about squat. <hr>";
 	}
 }
-echo "<hr>";
+echo "<br><hr>";
 if ($exercise == 'Burpee' or $exercise == 'Total Exercises'){
+	echo "<strong>Burpee</strong> <hr>"
 	if(isset($result3) && $result3->num_rows > 0){
 	    while($row = $result3->fetch_assoc()){
-	        echo "Date: ".$row['date']."<br>";
+	        echo "Date: ".$row['date']." ";
 	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
 	        echo "Total count: ".$row['total']."<hr>"; 
 	    }
 	}
 	else{
-	    echo "No data about Burpee.";
+	    echo "No data about Burpee. <hr>";
 	}
 }
 
