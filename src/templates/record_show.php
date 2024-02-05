5b016934aa484466060d9519c58b6190ed3c4a22
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-02-05 13:56:42 +0900

    Update record_show.php

--- src/templates/record_show.php
+++ src/templates/record_show.php
@@ -21,14 +21,15 @@ $result2 = $conn->query($sql2);
 $sql3 = "SELECT * FROM total_burpee WHERE name = '$name' ORDER BY date DESC, start_time DESC LIMIT $count";
 $result3 = $conn->query($sql3);
 
-echo "Data of " .$name. "<hr><hr><br>";
+$str= "Exercise Data of " .$name. "<hr><hr>";
+echo "<span style='font-size: 25px'>$str</span>";
     
 if ($exercise == 'PushUp' or $exercise == 'Total Exercises'){
-	echo "<strong>Push up</strong> <hr>"
+	echo "<strong>Push up</strong> <hr>";
 	if(isset($result1) && $result1->num_rows > 0){
 	
 	    while($row = $result1->fetch_assoc()){
-	        echo "Date: ".$row['date']." ";
+	        echo "Date: ".$row['date']."<br>";
 	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
 	        echo "Total count: ".$row['total']."<hr>"; 
 	    }
@@ -39,30 +40,30 @@ if ($exercise == 'PushUp' or $exercise == 'Total Exercises'){
 }
 echo "<br><hr>";
 if ($exercise == 'Squat' or $exercise == 'Total Exercises'){
-	echo "<strong>Squat</strong> <hr>"
+	echo "<strong>Squat</strong> <hr>";	
 	if(isset($result2) && $result2->num_rows > 0){
 	    while($row = $result2->fetch_assoc()){
-	        echo "Date: ".$row['date']." ";
+	        echo "Date: ".$row['date']."<br>";
 	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
 	        echo "Total count: ".$row['total']."<hr>"; 
 	    }
 	}
 	else{
-	    echo "No data about squat. <hr>";
+	    echo "No data about squat.<hr>";
 	}
 }
 echo "<br><hr>";
 if ($exercise == 'Burpee' or $exercise == 'Total Exercises'){
-	echo "<strong>Burpee</strong> <hr>"
+	echo "<strong>Burpee</strong> <hr>";	
 	if(isset($result3) && $result3->num_rows > 0){
 	    while($row = $result3->fetch_assoc()){
-	        echo "Date: ".$row['date']." ";
+	        echo "Date: ".$row['date']."<br>";
 	        echo "Time: From ".$row['start_time']." to ".$row['end_time']."<br>";
 	        echo "Total count: ".$row['total']."<hr>"; 
 	    }
 	}
 	else{
-	    echo "No data about Burpee. <hr>";
+	    echo "No data about Burpee.<hr>";
 	}
 }
 
