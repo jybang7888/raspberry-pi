cc0615be6c5b78f41dbad57653ac3816e9dd0213
royjunsik <155436935+royjunsik@users.noreply.github.com>
2024-02-08 15:25:17 +0900

    Create ranking_daily.php

---
+++ src/templates/ranking_daily.php
@@ -0,0 +1,49 @@
+<?php
+$servername = "192.168.1.249";
+$username = "root";
+$password = "1234";
+$dbname = "health";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql1 = "SELECT * FROM total_pushup ORDER BY total DESC";
+$result1 = $conn->query($sql1);
+$count1 = 1;
+
+$sql2 = "SELECT * FROM total_squat ORDER BY total DESC";
+$result2 = $conn->query($sql2);
+$count2 = 1;
+
+$sql3 = "SELECT * FROM total_burpee ORDER BY total DESC";
+$result3 = $conn->query($sql3);
+$count3 = 1;
+
+$str= "Daily Ranking <hr><hr>";
+echo "<span style='font-size: 25px'>$str</span>";
+
+if (1){
+	echo "<strong>Push up</strong> <hr>";
+  echo "    Rank     |     Name    |   Total count    <hr>";      
+	if(isset($result1) && $result1->num_rows > 0){
+	
+	    while($row = $result1->fetch_assoc()){
+            printf("%10d", $count1);
+            printf("%10s", $row['name']);
+            printf("%10d<hr>", $row['total']); 
+            $count1 = $count1 + 1;
+	    }
+	}
+	else{
+	    echo "No data about pushup. <hr>";
+	}
+}
+echo "<br><hr>";
+
+
+$conn->close();
+
+?>
